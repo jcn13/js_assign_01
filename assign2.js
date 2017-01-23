7c114991b57147421c6c7ba0a28e40d294cb05d1
jcn13 <jcn13@uol.com.br>
2017-01-22 18:01:10 -0800

    'twicked'

--- assign2.js
+++ assign2.js
@@ -1,7 +1,4 @@
 const myVal = process.argv
-let myArr = []
-myArr.push(Number(myVal[2]),Number(myVal[3]))
-console.log(myArr)
-let sum = myArr[0]+myArr[1]
-myArr.push(sum)
-console.log(myArr)
+console.log(myVal[2])
+let sum = (Number(myVal[2])+Number(myVal[3]))
+console.log(sum)
--- assign3.js
+++ assign3.js
@@ -1,13 +1,12 @@
 const myVal = process.argv
-let myArr = []
-myArr.push(Number(myVal[2]),Number(myVal[3]))
-let sum = myArr[0]+myArr[1]
+let sum = (Number(myVal[2])+Number(myVal[3]))
 let text = null
-if(sum>=10&&sum<100){
+if(sum>10&&sum<100)
+{
 	text = " is greater than 10 and less than 100 "
 }
-else if(sum>=100&&sum<1000){
+else if(sum>100&&sum<1000)
+{
 	text =" is greater than 100 and less than 1000"
 }
 console.log(sum + text)
-console.log(myArr)
